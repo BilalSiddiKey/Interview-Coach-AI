e39c6604b4de3778f1e4d0972bad7b02fe2c5ed3
BilalSiddiKey <mohammad.66169@iqra.edu.pk>
2026-06-16 12:10:52 +0500

    Update app.py

--- app.py
+++ app.py
@@ -26,6 +26,47 @@ def load_models():
 
 embedding_model, tokenizer, model = load_models()
 
+@st.cache_resource
+def load_documents():
+
+    documents = []
+
+    data_folder = "data"
+
+    for filename in os.listdir(data_folder):
+
+        if filename.endswith(".pdf"):
+
+            pdf_path = os.path.join(
+                data_folder,
+                filename
+            )
+
+            reader = PdfReader(pdf_path)
+
+            pdf_text = ""
+
+            for page in reader.pages:
+
+                text = page.extract_text()
+
+                if text:
+                    pdf_text += text + "\n"
+
+            documents.append({
+                "source": filename,
+                "content": pdf_text
+            })
+
+    return documents
+
+
+documents = load_documents()
+
+st.success(
+    f"Knowledge Base Loaded: {len(documents)} PDFs"
+)
+
 st.set_page_config(
     page_title="Interview Coach AI",
     page_icon="🎯",
